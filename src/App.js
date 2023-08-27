bfd8766381519a356ff81196a9791dfdc88d0506
Tang keke <hyou.fumis32861@gmail.com>
2023-08-27 22:07:13 +0900

    implemented first-level router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import './App.css';
+import React from "react"
+import {NavLink, Routes, Route} from "react-router-dom"
+import About from "./pages/About"
+import Home from "./pages/Home"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="row">
+        <div className="col-xs-offset-2 col-xs-8">
+          <div className="page-header"><h2>React Router Demo</h2></div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-2">
+          <div className="list-group">
+            {/*Router link*/}
+            <NavLink className="list-group-item" to="/about">About</NavLink>
+            <NavLink className="list-group-item active" to="/home">Home</NavLink>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="panel">
+            <div className="panel-body">
+                {/* register router */}
+                <Routes>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/home" element={<Home/>}/>
+                </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
